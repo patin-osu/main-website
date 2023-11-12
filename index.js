8afb4e984ceb0c0d5d22d981f725c695fc8c5895
Patin Muangjan <patin@patin.dev>
2023-11-12 21:45:37 +0700

    That's a mess of code

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-alert('Tap/click anywhere to listen to song')
-
 const parallaxContainer = document.querySelector('.parallax-container');
 const parallaxBackgrounds = document.querySelectorAll('.parallax-background');
 
@@ -19,4 +17,24 @@ parallaxContainer.addEventListener('mousemove', (event) => {
 
         parallaxBackground.style.transform = `translate3d(${parallaxOffset * -20}px, ${parallaxOffsetY * -20}px, 0) scale(1.1)`;
     }
+});
+
+const soundFile = new Audio('zenith.mp3');
+
+const initvol = 0.5; // Set the initial volume between 0 and 1
+
+document.addEventListener('click', (event) => {
+    if (event.target === document.body) {
+        // Play the sound when clicking anywhere on the document body
+        soundFile.play();
+    }
+});
+
+document.addEventListener('mousewheel', (event) => {
+    const delta = event.deltaY / 120; // Calculate the scroll wheel delta
+
+    let newVolume = initvol + delta;
+    newVolume = Math.max(0, Math.min(1, newVolume)); // Clamp the volume between 0 and 1
+
+    soundFile.volume = newVolume;
 });
