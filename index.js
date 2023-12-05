473712b14c04ab5dc03a2429b13e7d168b96de17
Patin Muangjan <patinopsu@gmail.com>
2023-12-05 18:51:24 +0700

    Add a footer and fix some grammar mistake

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const playButton = document.getElementById('playButton');
 const audioPlayer = document.getElementById('audioPlayer');
 
-// Add a click event listener to the button
 playButton.addEventListener('click', function() {
-  // Check if the audio is currently paused or has ended
   if (audioPlayer.paused || audioPlayer.ended) {
-    // Play the audio
     audioPlayer.play();
     playButton.textContent = 'Pause!';
   } else {
-    // Pause the audio
     audioPlayer.pause();
     playButton.textContent = 'Play!';
   }
